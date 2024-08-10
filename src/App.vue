0963bdf659004417f27a305f6bdd5031279e2fe9
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2024-08-10 10:36:25 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup> // tambahkan setup untuk bisa mengakses variabel
+// const hello = "hello";
+// utk mengakses ini perlu v-bind
+// const green = "green"; 
+// console.log(hello);
+import FormTodo from './components/FormTodo.vue'; // ini menggunakan component
+import { ref } from 'vue';  // import dlu agar bisa menggunakan ref
+
+// Ketika datanya berubah maka vue akan mengupdate domnya
+const todos = ref([]); // ini namanya state, bukan variable lagi
+
+function add(data) { // harus sama dengan yang ada di FormTodo
+  // console.log(data.todo); // todo dapat dari FormTodo.vue
+  todos.value.push({ id: crypto.randomUUID(), todo: data.todo });
+  data.clear(); // ketika datanya berhasil ditambahkan, clear formnya
+}
+</script>
+
+<template>
+  <!-- <h1 v-bind:style="{color: green}">Hello World {{ hello }}</h1> -->
+   <section class="wrapper">
+    <FormTodo  @add-todo="add"/> <!-- ini menggunakan component -->
+   </section>
+</template>
+
+ <!-- scoped ini dia menjadi tidak global, kalo tidak ada scoped dia global --> 
+<style scoped> 
+  /* .warna{
+    color: blue;
+  } */
+   .wrapper{
+    display: flex;
+    flex-direction: column;
+    height: 100svh;
+    justify-content: center;
+    align-items: center;
+   }
+</style>
---
+++ src/components/FormTodo.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'; 
+const todo = ref('');
+
+function clear() { // mnghapus data formnya ketika data todonya berhasil di tambahkan
+    todo.value = "";
+    }
+</script> 
+
+<template>
+    <form @:submit.prevent="$emit('addTodo', {todo, clear})">
+        <h1 class="title">Todo</h1>
+        <div class="form-control">
+            <label for="todo">Todo :</label>
+            <input type="text" name="" v-model="todo" id="todo" placeholder="your todo...." required>
+        </div>
+        <div style="text-align: end;">
+            <button>Add</button>
+        </div>
+    </form>
+</template>
